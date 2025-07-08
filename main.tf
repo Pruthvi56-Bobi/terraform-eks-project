ff1b59e5161506e9bf75c0641373276bb0a47999
Ubuntu <ubuntu@ip-172-31-89-8.ec2.internal>
2025-07-08 13:27:44 +0000

    Removed IAM role creation to fix duplication issue

--- main.tf
+++ main.tf
@@ -4,41 +4,20 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# IAM Role for EKS
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach EKS Cluster Policy
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-# Use existing subnets for the EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = "arn:aws:iam::774305598336:role/eks_cluster_role"
 
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+}
 
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy
-  ]
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
